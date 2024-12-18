24e15b9872fc7f406abdc0976f271899ae12c0e9
arnau2104 <amarques20733@iesjoanramis.org>
2024-12-18 21:10:41 +0100

    servidor json fet

--- SERVIDOR_JSON/script.js
+++ SERVIDOR_JSON/script.js
@@ -3,6 +3,12 @@ const urlPalabras = "http://localhost:3000/palabras";
 const url = window.location.pathname;
 if(url.includes("palabras-list.html")){
     cargarPalabras();
+}else if (url.includes("palabras-edit.html")){
+    const parametros = new URLSearchParams(window.location.search);
+    const palabraId = parametros.get("id");
+    if(palabraId) {
+        cargarPalabra(palabraId);
+    }
 }
 
 document.getElementById("palabra-form")?.addEventListener('submit', guardarPalabra);
@@ -10,12 +16,13 @@ document.getElementById("palabra-form")?.addEventListener('submit', guardarPalab
 async function guardarPalabra(e) {
     e.preventDefault();
 
-    const metodo = "POST";
+    const id = new URLSearchParams(window.location.search).get("id");
+    const metodo = id ? "PUT" : "POST";
     
-    const url = urlPalabras;
+    const urlGuardar = id ? `${urlPalabras}/${id}` : urlPalabras;
 
      const palabra = {
-        id: 0,
+        id: "0",
         palabra: document.getElementById("palabra").value,
         dificultad : +document.getElementById("dificultad").value
      }
@@ -24,11 +31,12 @@ async function guardarPalabra(e) {
      .then((response)=> response.json())
      .then((data)=>{
         if(data && data.length > 0){
-        palabra.id = +data[data.length - 1].id + 1;
+       let idAux = palabra.id = +data[data.length - 1].id + 1;
+       palabra.id = idAux + "";
         }else {
-          palabra.id = +1;  
+            palabra.id = "1";
         }
-        return fetch(`${url}`, {
+        return fetch(`${urlGuardar}`, {
             method: metodo,
             body: JSON.stringify(palabra),
             headers: {
@@ -61,4 +69,36 @@ async function cargarPalabras() {
     }catch(error) {
         console.log("Error al cargar las palabras: ", error);
     }
+}
+
+async function cargarPalabra(id) {
+    fetch(urlPalabras)
+    .then((respose)=> respose.json())
+    .then((data)=>{
+        const palabra = data.find((palabraP)=>palabraP.id == id)
+        document.getElementById("palabra").value = palabra.palabra;
+        document.getElementById("dificultad").value = palabra.dificultad;
+    })
+    .catch((error)=> console.log("Error al cargar la palabra", error));
+}
+
+
+async function eliminarPalabra(id) {
+    const urlDelete = `${urlPalabras}/${id}`;
+    // console.log(urlDelete);
+
+    fetch(`${urlDelete}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "aplication/json"
+        }
+    })
+    .then((response)=>response.json())
+    .then(()=>cargarPalabras())
+    .catch((error)=> console.log("Error al eliminar la palabra: ", error ))
+}
+
+
+async function editarPalabra(id) {
+    window.location.href = `palabras-edit.html?id=${id}`;
 }
