22f90a6578af44b5b97c4f3bf8e5233e59aa7dc0
arnau2104 <amarques20733@iesjoanramis.org>
2025-01-15 19:55:20 +0100

    react

---
+++ react/mi-primera-react-app/src/components/Titulo.js
@@ -0,0 +1,10 @@
+export default function Titulo({titulo, subTitulo}) {
+    return (
+        <>
+            <h1 className="titulo"> {titulo} </h1>
+            <br/>
+            <h2 className="subtitulo"> {subTitulo} </h2>
+        </>
+    )
+}
+
